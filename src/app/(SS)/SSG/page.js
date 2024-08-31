6d9035eda6b20ee155a4efd7d60627340f71772a
JS Project <mahmoudveteran@gmail.com>
2024-08-31 06:53:44 +0300

    for deploy

--- src/app/(SS)/SSG/page.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-// // nothing to do the default behavior for next.js
-// const page = () => {
-//   return (
-//     <div>page</div>
-//   )
-// }
-
-// export default page
